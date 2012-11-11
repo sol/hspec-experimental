106acdbdd5b9b67a47e2a829d0600c4c2665008b
Simon Hengel <sol@typeful.net>
2012-11-11 14:57:30 +0100

    Do not rely on modifications to Hspec

--- src/Test/Hspec/Experimental.hs
+++ src/Test/Hspec/Experimental.hs
@@ -6,26 +6,38 @@ module Test.Hspec.Experimental (
 ) where
 
 import           Test.Hspec hiding (it)
-import           Test.Hspec.Core (Example(..), Params(..), SpecTree(..), fromSpecList)
+import           Test.Hspec.Core (Params(..), Result(..), SpecTree(..), fromSpecList)
 
 import qualified Control.Exception as E
 import           Test.HUnit.Lang (HUnitFailure(..))
 
-import qualified Test.QuickCheck.Property as QC
-import qualified Test.QuickCheck as QC (quickCheckWithResult)
+import qualified Test.QuickCheck.Property as QCP
+import qualified Test.QuickCheck as QC
 
 runExpectation :: Expectation -> QC.Property
-runExpectation action = QC.morallyDubiousIOProperty $ do
+runExpectation action = QCP.morallyDubiousIOProperty $ do
   (action >> return succeeded) `E.catch` \(HUnitFailure err) -> return (failed err)
   where
-    succeeded  = QC.property QC.succeeded
-    failed err = QC.property QC.failed {QC.reason = err}
+    succeeded  = QC.property QCP.succeeded
+    failed err = QC.property QCP.failed {QCP.reason = err}
 
 instance QC.Testable Expectation where
   property     = runExpectation
   exhaustive _ = True
 
 it :: QC.Testable a => String -> a -> Spec
-it s e = fromSpecList [SpecItem s result]
+it s p = fromSpecList [SpecItem s result]
   where
-    result c = QC.quickCheckWithResult (paramsQuickCheckArgs c) e >>= evaluateExample c
+    result c = do
+      -- copied from Test.Hspec.Core.Type
+      r <- QC.quickCheckWithResult (paramsQuickCheckArgs c) p
+      return $
+        case r of
+          QC.Success {}               -> Success
+          f@(QC.Failure {})           -> Fail (QC.output f)
+          QC.GaveUp {QC.numTests = n} -> Fail ("Gave up after " ++ quantify n "test" )
+          QC.NoExpectedFailure {}     -> Fail ("No expected failure")
+
+quantify :: Int -> String -> String
+quantify 1 s = "1 " ++ s
+quantify n s = show n ++ " " ++ s ++ "s"
